53b762560783e683627c905aa92a20c8889684b2
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-02-15 14:00:40 +0100

    added links to mangomint

--- src/app/features/services/services-grid/services-grid.ts
+++ src/app/features/services/services-grid/services-grid.ts
@@ -4,24 +4,41 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-services-grid',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './services-grid.html',
   styleUrl: './services-grid.css',
 })
 export class ServicesGrid {
   protected readonly text = UI_TEXT;
-   content = {
+  content = {
     services: {
       title: 'Our Services',
       subtitle: 'What We Do',
       btnBook: 'Book Appointment',
       btnAll: 'See All Services',
       cards: [
-        { title: 'Hair Extensions', img: 'assets/images/servicesHairHero.jpg' },
-        { title: 'Treatments', img: 'assets/images/hairKinetics.png' },
-        { title: 'Color', img: 'assets/images/servicesHairHero.jpg' },
-        { title: 'Styling', img: 'assets/images/stylingService.png' }
-      ]
+        {
+          title: 'Hair Extensions',
+          img: 'assets/images/servicesHairHero.jpg',
+          bookingUrl: 'https://booking.mangomint.com/307273?showOnlyScId=6',
+        },
+        {
+          title: 'Treatments',
+          img: 'assets/images/hairKinetics.png',
+          bookingUrl: 'https://booking.mangomint.com/307273?showOnlyScId=9',
+        },
+        {
+          title: 'Color',
+          img: 'assets/images/servicesHairHero.jpg',
+          bookingUrl: 'https://booking.mangomint.com/307273?showOnlyScId=5',
+        },
+        {
+          title: 'Styling',
+          img: 'assets/images/stylingService.png',
+          bookingUrl: 'https://booking.mangomint.com/307273?showOnlyScId=11',
+        },
+      ],
     },
     brands: {
       title: 'SHOP BRANDS WE LOVE',
@@ -30,9 +47,20 @@ export class ServicesGrid {
         { name: 'Davines', img: 'assets/svg/davines.svg' },
         { name: 'AZ', img: 'assets/svg/craftLuxury.svg' },
         { name: 'Nutrafol', img: 'assets/svg/nutrafol.svg' },
-        { name: "L'ANZA", img: 'assets/svg/lanza.svg' }
-      ]
-    }
+        { name: "L'ANZA", img: 'assets/svg/lanza.svg' },
+      ],
+    },
   };
 
+  openBooking(url: string) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+    a.style.display = 'none';
+
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  }
 }
