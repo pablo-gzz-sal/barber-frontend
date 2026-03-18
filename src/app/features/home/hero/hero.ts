b3016125f4b2fef060d2f17a372921d54e05c97e
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-03-17 18:00:20 -0700

    added new animation

--- src/app/features/home/hero/hero.ts
+++ src/app/features/home/hero/hero.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { UI_TEXT } from '../../../core/constants/app-text';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-hero',
@@ -8,7 +9,8 @@ import { UI_TEXT } from '../../../core/constants/app-text';
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {
+export class Hero implements AfterViewInit {
+  @ViewChild('heroText') heroText!: ElementRef;
   protected readonly text = UI_TEXT;
   content = {
     hero: {
@@ -16,5 +18,34 @@ export class Hero {
       line2: 'BEAUTY IN BALANCE'
     }
   }
+
+ngAfterViewInit() {
+  const lines = this.heroText.nativeElement.querySelectorAll('.hero-line');
+
+  lines.forEach((line: Element, lineIndex: number) => {
+    const text = line.textContent?.trim() || '';
+    line.innerHTML = text.split('').map(char =>
+      char === ' '
+        ? '<span class="inline-block">&nbsp;</span>'
+        : `<span class="inline-block">${char}</span>`
+    ).join('');
+
+    const chars = Array.from(line.querySelectorAll('span'));
+    const mid = Math.floor(chars.length / 2);
+
+    gsap.from(chars, {
+      opacity: 0,
+      filter: 'blur(12px)',
+      y: 20,
+      duration: 1.8,
+      ease: 'power3.out',
+      stagger: {
+        each: 0.06,
+        from: mid,
+      },
+      delay: 0.2 + lineIndex * 0.25,
+    });
+  });
+}
 
 }
