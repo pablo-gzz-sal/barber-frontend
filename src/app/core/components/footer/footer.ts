c2c76f6f039a42f9ffd056cab43a8bb30e21d413
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-03-20 15:28:12 -0600

    added router link in footer

--- src/app/core/components/footer/footer.ts
+++ src/app/core/components/footer/footer.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UI_TEXT } from '../../constants/app-text';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   standalone: true,
   templateUrl: './footer.html',
   styleUrl: './footer.css',
