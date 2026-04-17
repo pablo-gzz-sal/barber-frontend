d30de05e7ed2545fc2b49d6924519e9db02f8ee0
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-04-17 15:53:46 -0600

    updated prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   name: 'PROD',
-  apiUrl: 'https://barber-api-j0aa.onrender.com/v1',
+  apiUrl: 'https://headlessbackendbarber.onrender.com/v1',
   shopifyEndpoint: '/shopify',
   mangomintEndpoint: '/mangomint',
   shopifyStorefrontUrl: 'https://josephbattisti-com.myshopify.com',
