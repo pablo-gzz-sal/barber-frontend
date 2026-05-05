a4825534eb3102f529a40cc0888f76be026ea9e1
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-05 17:51:28 -0600

    sort brands

--- src/app/features/shop/shop-brands/shop-brands.ts
+++ src/app/features/shop/shop-brands/shop-brands.ts
@@ -132,7 +132,7 @@ export class ShopBrands {
     //   link: 'white-logo',
     //   dark: true,
     // },
-  ];
+  ].sort((a, b) => a.name.localeCompare(b.name));
 
   onBrand(link: string) {
     if (link === 'milbon') {
